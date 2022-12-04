8fe502a5a037af536d1e6bea539ea58d21b70459
anya.shan <anya.shanaida@yahoo.com>
2022-12-04 13:15:59 +0200

    add redirect to pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 
 import { Layout } from 'components/Layout/Layout';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/operations';
@@ -26,9 +28,24 @@ export function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/contacts" element={<Contacts />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute component={Register} redirectTo="/contacts" />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute component={Login} redirectTo="/contacts" />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute component={Contacts} redirectTo="/login" />
+              }
+            />
             <Route path="*" element={<Home />} />
           </Route>
         </Routes>
